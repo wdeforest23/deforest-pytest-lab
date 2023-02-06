1f57aed4db07a9b250e6a9bc5a74f8eaadbb67e3
Lucas <lucaswelch7@gmail.com>
2023-02-05 21:22:30 -0800

    Hi Defo2

--- Fixme.py
+++ Fixme.py
@@ -2,7 +2,7 @@
 
 
 def evens(n):
-    return list(filter(lambda x: x % 2 == 0, map(lambda x: x, range(n+1))))
+    return list(filter(lambda x: x % 2 == 0, map(lambda x: x, range(n + 1))))
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
